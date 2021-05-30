22a786930921448c28ce2d017f2699d4a304c22a
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2021-05-30 14:50:53 +0600

    init

---
+++ app/src/main/java/com/fundinghelp/moniruzzamanshadhinapplication/BkashActivity.kt
@@ -0,0 +1,11 @@
+package com.fundinghelp.moniruzzamanshadhinapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BkashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bkash)
+    }
+}
---
+++ app/src/main/java/com/fundinghelp/moniruzzamanshadhinapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.fundinghelp.moniruzzamanshadhinapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonAction()
+    }
+
+    private fun buttonAction() {
+        btnRegCustomer.setOnClickListener {
+            val intent = Intent(this@MainActivity, BkashActivity::class.java)
+            startActivity(intent);
+        }
+    }
+}
